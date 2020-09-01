49c1922c59a591ade2b80c16b769b88a5459bf5d
Anksharskarp <58315985+Anksharskarp@users.noreply.github.com>
2020-08-31 22:16:03 -0400

    Update Hello.java

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -2,7 +2,9 @@ public class Hello {
 
     public static void main(String[] args) {
         // generate some simple output
+        // created by William Z
         System.out.println("Hello, World!");
+        System.out.println("How are you?");
     }
 
 }
